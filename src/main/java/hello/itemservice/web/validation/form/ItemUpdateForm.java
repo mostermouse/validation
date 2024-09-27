33360a4378a3c6c85d72dccc2338eb4893b4f7fe
mostermouse <whdls7946@gmail.com>
2024-09-27 16:54:54 +0900

    bean validation

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,19 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+@Data
+public class ItemUpdateForm {
+    @NotBlank
+    private String itemName;
+    @NotNull
+    @Range(min = 100 , max = 1000000)
+    private Integer price;
+
+    private Integer quantity;
+
+}
